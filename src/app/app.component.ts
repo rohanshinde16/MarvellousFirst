8ff4132e022c07c848ec1345df7f9c6e4154807e
“Rohan <balu.shinde@vowellms.com>
2018-12-19 15:29:41 +0530

    5th assignment Commit 1st and 2nd task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div ng-app>
+  <h1>Inline Format</h1>
+             <p>Marvellous Infosystems : <input type="text" name="First"></p>
+              </div>
+              `,
+  styles: ['p{color:blue}']
 })
 export class AppComponent {
   title = 'MarvellousFirst';
